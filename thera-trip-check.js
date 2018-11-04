49afd57ccad42a15540fd0afb3c2b9610b54caa6
Zheng Tian <zheng718@gmail.com>
2018-11-04 13:17:28 -0800

    handle bogus start system; print systems along recommended routes

--- thera-trip-check.js
+++ thera-trip-check.js
@@ -1,26 +1,32 @@
 if (process.argv.length < 3) {
   // TODO: system avoidance
+  // TODO: choice of checking hubs or input
   console.log('usage: theraTripCheck.js [start system]')
   process.exit(1);
 }
 
 const start = process.argv[2].toUpperCase();
 console.log('theraTripCheck: running check for system ' + start);
-
-const hubs = ["Jita", "Amarr", "Dodixie"];
+var startSystem;
 
 const EVEoj = require("EVEoj");
-// var SDD = EVEoj.SDD.Create("json", {path: "D:/Documents/staticevedata"});
 var SDD = EVEoj.SDD.Create("json", {path: "staticevedata"});
 var map;
 SDD.LoadMeta().then(function() {
   map = EVEoj.map.Create(SDD, "K");
   return map.Load();
+}).then(function() { // handle bogus system name
+  startSystem = map.GetSystem({name: start});
+  if (startSystem == null) {
+    console.log('error: ' + start + ' is not a recognized system.');
+    process.exit(1);
+  }
 });
 
 const http = require('http');
 const https = require('https');
 
+const hubs = ["Jita", "Amarr", "Dodixie"];
 const req = https.get('https://www.eve-scout.com/api/wormholes', (res) => {
   // console.log(`STATUS: ${res.statusCode}`);
   // console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
@@ -46,22 +52,24 @@ function getJumps(evescoutjson) { // TODO: jump off-by-1 because of thera jump
   var startSystem = map.GetSystem({name: start});
 
   // calc shortest path from start to thera
-  var shortestPreThera = Infinity;
-  var closestPreThera;
+  var minjumps = Infinity;
+  var closestConnection;
+  var shortestPreRoute;
   for (var entry of theraholes) {
     var theraConnection = entry.destinationSolarSystem;
 
     var route = map.Route(startSystem.ID, theraConnection.id, [], false, false);
     // console.log(theraConnection.name + " " + theraConnection.id);
-    console.log(theraConnection.name + " " + theraConnection.id + ": " + route.length)
-    if (route.length < shortestPreThera && (route != 0 || startSystem.ID == theraConnection.id)) {
-      shortestPreThera = route.length;
-      closestPreThera = theraConnection;
+    console.log(theraConnection.name + " " /*+ theraConnection.id*/ + ": " + route.length)
+    if (route.length < minjumps && (route != 0 || startSystem.ID == theraConnection.id)) {
+      minjumps = route.length;
+      closestConnection = theraConnection;
+      shortestPreRoute = route;
     }
 
     // console.log("http://eve-gatecheck.space/eve/#" + startSystem.name + ":" + theraConnection.name + ":shortest");
   }
-  console.log("closest Thera connection to start: " + closestPreThera.name + ", " + shortestPreThera + " jumps");
+  console.log("closest Thera connection to start: " + closestConnection.name + ", " + shortestPreRoute.length + " jumps");
   // TODO: wormhole info (sigs, EOL, size, etc)
 
   // calc shortest path from thera to each of the hubs
@@ -70,7 +78,7 @@ function getJumps(evescoutjson) { // TODO: jump off-by-1 because of thera jump
     var hubSystem = map.GetSystem({name: hub});
 
     var shortestKSpaceRoute = map.Route(startSystem.ID, hubSystem.ID, [], false, false);
-    if (shortestPreThera + 2 >= shortestKSpaceRoute) {
+    if (shortestPreRoute.length + 2 >= shortestKSpaceRoute) {
       console.log("shortest route including Thera longer than shortest k-space route to " + hub);
       
       // TODO: do i really want to continue? what if thera route slightly longer but much safer?
@@ -79,55 +87,51 @@ function getJumps(evescoutjson) { // TODO: jump off-by-1 because of thera jump
       continue;
     }
 
-    var shortestPostThera = Infinity;
-    var closestPostThera;
+    var minjumps = Infinity;
+    var closestConnection;
+    var shortestPostRoute;
     for (var entry of theraholes) {
       var theraConnection = entry.destinationSolarSystem;
       // console.log(theraConnection);
 
       var route = map.Route(theraConnection.id, hubSystem.ID, [], false, false);
-      console.log(theraConnection.name + " " + theraConnection.id + ": " + route.length)
-      if (route.length < shortestPostThera && (route.length != 0 || theraConnection.id == hubSystem.ID)) {
-        shortestPostThera = route.length;
-        closestPostThera = theraConnection;
+      console.log(theraConnection.name + " " /*+ theraConnection.id*/ + ": " + route.length)
+      if (route.length < minjumps && (route.length != 0 || theraConnection.id == hubSystem.ID)) {
+        minjumps = route.length;
+        closestConnection = theraConnection;
+        shortestPostRoute = route;
       }
 
       // console.log("http://eve-gatecheck.space/eve/#" + theraConnection.name + ":" + hub + ":shortest");
     }
-    // console.log("shortestPreThera: " + shortestPreThera + " shortestPostThera: " + shortestPostThera)
-    console.log("shortest route length via Thera: " + (shortestPreThera + shortestPostThera) + " jumps");
+    // console.log("shortest jumps pre-Thera: " + shortestPreRoute.length + " shortest jumps post-Thera: " + shortestPostRoute.length)
+    console.log("shortest route length via Thera: " + (shortestPreRoute.length + shortestPostRoute.length) + " jumps");
 
-    console.log("closest Thera connection to " + hub + ": " + closestPreThera.name + ", " + shortestPostThera + " jumps");
+    console.log("closest Thera connection to " + hub + ": " + closestConnection.name + ", " + shortestPostRoute.length + " jumps");
     // TODO: wormhole info (sigs, EOL, size, etc)
 
-    if (shortestPreThera + 2 + shortestPostThera >= shortestKSpaceRoute.length) {
+    if (shortestPreRoute.length + 2 + shortestPostRoute.length >= shortestKSpaceRoute.length) {
       console.log("shortest Thera route longer than shortest k-space route to " + hub);
       
       // TODO: provide comparison betw routes (more details)
 
     }
+
+    // print out systems along route
+    console.log("suggested route from " + start + " to " + hub)
+    for (var systemid of shortestPreRoute) {
+      console.log(map.GetSystem({id: systemid}).name);
+    }
+    console.log("Thera"); // TODO: add entrance/exit wormhole info before/after
+    for (var systemid of shortestPostRoute) {
+      console.log(map.GetSystem({id: systemid}).name);
+    }
   }
 
 }
 
-// wont work, need to use .id
-// helper function
-// function getRouteToThera(start, end) {
-//   for (var entry of theraholes) {
-//     var currSystem = entry.destinationSolarSystem.name;
-//     console.log(currSystem);
-
-//     var route = map.Route(startSystem.ID, entry.destinationSolarSystem.ID, [], false, false);
-//     if (route.length < shortestPreThera) {
-//       shortestPreThera = route.length;
-//     }
-
-//     console.log("http://eve-gatecheck.space/eve/#" + currSystem + ":" + hub + ":shortest");
-//   }
-// }
-
 // TODO: zkill checks here.
-// TODO: check thera itself too
+// TODO: check starting system and thera too
 function checkZKill(system) {
   //
 }
